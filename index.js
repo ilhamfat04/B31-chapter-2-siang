ca1c7f365c3f954f06cb0be87eb37f41e1cfa5a1
= <=>
2022-02-10 14:12:07 +0700

    update

--- index.js
+++ index.js
@@ -14,6 +14,30 @@ app.use(express.urlencoded({ extended: false }))
 // false => belum login
 const isLogin = true
 
+const blogs = [
+    {
+        title: "Pasar Coding di Indonesia Dinilai Masih Menjanjikan",
+        content: "Ketimpangan sumber daya manusia (SDM) di sektor digital masih menjadi isu yang belum terpecahkan. Berdasarkan penelitian ManpowerGroup, ketimpangan SDM global, termasuk Indonesia, meningkat dua kali lipat dalam satu dekade terakhir. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, molestiae numquam! Deleniti maiores expedita eaque deserunt quaerat! Dicta, eligendi debitis?",
+        author: "Ichsan Emrald Alamsyah",
+        posted_at: "12 Jul 2021 22:30 WIB"
+    }
+]
+
+let month = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+]
+
 // Set endpoint
 app.get('/', function (req, res) {
     res.send("Hello World")
@@ -24,7 +48,17 @@ app.get('/home', function (req, res) {
 })
 
 app.get('/blog', function (req, res) {
-    res.render('blog', { isLogin: isLogin })
+
+    console.log(blogs) // hanya ada 4 properti
+
+    let dataBlogs = blogs.map(function (data) {
+        return {
+            ...data,
+            isLogin: isLogin
+        }
+    })
+
+    res.render('blog', { isLogin: isLogin, blogs: dataBlogs })
 })
 
 app.get('/add-blog', function (req, res) {
@@ -34,9 +68,19 @@ app.get('/add-blog', function (req, res) {
 app.post('/blog', function (req, res) {
     let title = req.body.title
     let content = req.body.content
+    let date = new Date()
 
-    console.log(`Data title : ${title}`);
-    console.log(`Data content : ${content}`);
+    let blog = {
+        title: title,
+        content,
+        author: "Ichsan Emrald Alamsyah",
+        posted_at: getFullTime(date)
+    }
+
+    blogs.push(blog)
+
+    res.redirect('/blog')
+
 })
 
 app.get('/blog/:id', function (req, res) {
@@ -55,3 +99,18 @@ const port = 5000
 app.listen(port, function () {
     console.log(`Server running on port ${port}`);
 })
+
+function getFullTime(time) {
+    let date = time.getDate()
+    let monthIndex = time.getMonth()
+    let year = time.getFullYear()
+
+    let hours = time.getHours()
+    let minutes = time.getMinutes()
+
+    if (minutes < 10) {
+        minutes = '0' + minutes
+    }
+
+    return `${date} ${month[monthIndex]} ${year} ${hours}:${minutes} WIB`
+}
